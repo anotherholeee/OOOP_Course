09bff3e465691e71f87ad065abe6796fd0bff748
anotherholeee <roikoartem@gmail.com>
2025-11-23 16:30:59 +0300

    Changes

--- vika_s_lab6/main.cpp
+++ vika_s_lab6/main.cpp
@@ -12,9 +12,36 @@ protected:
 public:
 User(): groupNum(0){};
 User(string fio, string faculty, int groupNum): fio(std::move(fio)), faculty(std::move(faculty)), groupNum(groupNum){};
-
 User(const User& copy): groupNum(copy.groupNum) {}
+
+    // ГЕТТЕРЫ (получить значение)
+     string getFio()  { return std::move(fio); }
+     string getFaculty()  { return std::move(faculty); }
+    int getGroupNum() const { return groupNum; }
+
+    // СЕТТЕРЫ (установить значение)
+    void setFio(const string& newFio) {
+    if (!newFio.empty()) {
+        fio = newFio;
+    }
+}
+
+    void setFaculty(const string& newFaculty) {
+    if (!newFaculty.empty()) {
+        faculty = newFaculty;
+    }
+}
+
+    void setGroupNum(int newGroupNum) {
+    if (newGroupNum > 0 && newGroupNum < 1000) {
+        groupNum = newGroupNum;
+    }
+}
+
+
 };
+
+
 
 class Answer: public User{ //дата выполнения теста, ФИО выполнившего тест
     private:
@@ -24,7 +51,12 @@ Answer(): date(0) {};
     Answer(const string& fio, const string& faculty, int groupNum, int date): User(fio, faculty, groupNum), date(date) {};
 Answer(const Answer& copy): User(copy.fio, copy.faculty, copy.groupNum), date(copy.date) {};
 
+int getDate() const { return date; }
+
+    void setDate(int newDate) { date = newDate; }
 };
+
+
 
 class Question { //Текст вопроса, варианты ответа, правильный ответ, баллы за вопрос
     private:
@@ -39,7 +71,38 @@ public:
     Question(string questionText, string  variants, string  rightVariant, int scores) : questionText(
         std::move(questionText)), variants(std::move(variants)), rightVariant(std::move(rightVariant)), scores(scores) {}
     Question(const Question& copy): questionText(copy.questionText),variants(copy.variants),rightVariant(copy.rightVariant),scores(copy.scores) {}
+
+    string getQuestionText() { return std::move(questionText); }
+    string getVariants() { return std::move(variants); }
+    string getRightVariant() { return std::move(rightVariant); }
+    int getScores() const { return scores; }
+
+    void setQuestionText(string&& newQuestionText) {  // Для перемещения
+        if (!newQuestionText.empty()) {
+            questionText = std::move(newQuestionText);
+        }
+    }
+
+    void setVariants(const string& newVariants) {
+        if (!newVariants.empty()) {
+            variants = newVariants;
+        }
+    }
+
+    void setRightVariant(const string& newRightVariant) {
+        if (!newRightVariant.empty()) {
+            rightVariant = newRightVariant;
+        }
+    }
+
+    void setScores(int newScores) {
+        if (newScores >= 0) {
+            scores = newScores;
+        }
+    }
 };
+
+
 
 class Result: public Answer {
 //ФИО, факультет, номер группы, результат(в %), сдал/не сдал
@@ -50,7 +113,21 @@ public:
     Result(): result(0), passed(false) {};
     Result(const string& fio, const string& faculty, int groupNum, int date, float result) : Answer(fio, faculty, groupNum, date), result(result), passed(result >= 60.0f) {}
 Result (const Result& copy): Answer(copy), result(copy.result), passed(copy.passed) {}
+
+float getResult() const { return result; }
+    bool getPassed() const { return passed; }
+
+    void setResult(float newResult) {
+        result = newResult;
+        passed = (result >= 60.0f);
+    }
+
+    void setPassed(bool newPassed) { passed = newPassed; }
+
+
 };
+
+
 
 class Test { //название теста, тема теста, перечень вопросов, перечень полученных ответов
     private:
@@ -68,6 +145,18 @@ Answer answers[MAX_ANSWERS];
     Test(): questions(), answers(), questionCount(), answerCount() {}
     Test(string testName, string testTheme): testName(std::move(testName)), testTheme(std::move(testTheme)), questionCount(0), answerCount(0) {}
     Test(const Test& copy): testName(copy.testName), testTheme(copy.testTheme), questionCount(0), answerCount(0) {}
+
+string getTestName() { return std::move(testName); }
+    string getTestTheme() { return std::move(testTheme); }
+
+    int getQuestionCount() const{ return questionCount; }
+    int getAnswerCount() const{ return answerCount; }
+
+    void setTestName(string newTestName) { testName = std::move(newTestName); }
+    void setTestTheme(string newTestTheme) { testTheme = std::move(newTestTheme); }
+    void setQuestionCount(int newCount) { questionCount = newCount; }
+    void setAnswerCount(int newCount) { answerCount = newCount; }
+
 };
 
 
